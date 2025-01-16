5c5af8879afa47fc399ac874b8af717b8d799d04
Alec Ellsworth <alec.ells123@gmail.com>
2025-01-16 13:18:06 -0600

    Basic consumer and update for urls and views

--- drinks/drinks/views.py
+++ drinks/drinks/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 @api_view(['GET', 'POST'])
-def drink_list(request):
+def drink_list(request, format=None):
 
     if request.method == 'GET':
         # get the drinks
@@ -23,7 +23,7 @@ def drink_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def drink_detail(request, id):
+def drink_detail(request, id, format=None):
 
     try:
         drink = Drink.objects.get(pk=id)
